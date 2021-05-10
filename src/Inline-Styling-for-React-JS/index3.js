87590ed4718ef4c3dfb3bf6ff73a369234c41a80
Amit Kumar <59612091+amitcsjec@users.noreply.github.com>
2021-05-10 15:05:16 +0530

    Inline Styling for React JS Update second commit

--- src/Inline-Styling-for-React-JS/index3.js
+++ src/Inline-Styling-for-React-JS/index3.js
@@ -1,22 +1,31 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const date = new Date();
+const date = new Date(2019, 1, 1, 19);
 const currentTime = date.getHours();
 
 // console.log(currentTime);
 
 let greeting;
 
+const customStyle = {
+  color: ""
+};
+
 if (currentTime < 12) {
   greeting = "Good Morning";
+  customStyle.color = "red";
 } else if (currentTime < 18) {
   greeting = "Good Afternoon";
+  customStyle.color = "green";
 } else {
   greeting = "Good Night";
+  customStyle.color = "blue";
 }
 
 ReactDom.render(
-  <h1 className="heading2">{greeting}</h1>,
+  <h1 className="heading2" style={customStyle}>
+    {greeting}
+  </h1>,
   document.getElementById("root")
 );
