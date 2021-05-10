4957e7fa93fc180a90ef636d563913d5a0e453dd
Amit Kumar <59612091+amitcsjec@users.noreply.github.com>
2021-05-10 12:28:23 +0530

    Second Commit JS expressions in JSX Practice

---
+++ src/index3.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamson</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
